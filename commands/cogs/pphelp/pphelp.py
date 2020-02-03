b63d3c0102a1a4d506b8d1274402640b43ad4eec
Quesamo <quesamovich@gmail.com>
2020-02-03 23:55:45 +0100

    Added trigger words, support for #non-tf2-listings

--- commands/cogs/pphelp/pphelp.py
+++ commands/cogs/pphelp/pphelp.py
@@ -10,8 +10,8 @@ class pphelp(commands.Cog):
     async def on_message(self, message):
 
         self.main_message = "Paypal/cash trades are a risk. Please take precautions before sending items to avoid being scammed. Such precautions include, but are not limited to, checking user's backpack.tf, steamrep, rep.tf, asking for screenshots of previous cash trades and asking other people who have worked with the user for legitimacy. Stay safe!"
-        self.listing_channels = [332221029382488065, 375800525054148609, 332221993724411944] #the channel IDs of all the server's listing channels (#low-tier-listings, #high-tier-listings, #unusual-listings)
-        self.trigger_words = ['paypal', 'cash', 'money']
+        self.listing_channels = [332221029382488065, 375800525054148609, 332221993724411944, 672620604683321375] #the channel IDs of all the server's listing channels (#low-tier-listings, #high-tier-listings, #unusual-listings, #non-tf2-listings)
+        self.trigger_words = ['paypal', 'pp', 'cash', 'money', 'btc', 'bitcoin']
 
         if message.author.id != 603734708450361364: #makes sure the listener isn't triggered by its own messages
             for channel_id in self.listing_channels: #cycles through each of the listing channels and sees if the message was sent in any of them
